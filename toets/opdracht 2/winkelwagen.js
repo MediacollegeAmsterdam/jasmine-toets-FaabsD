cfdb34c84b11e5c5cdb226520b87558e5a393f33
Fabian <19959@ma-web.nl>
2020-10-20 10:56:52 +0200

    object en methods aangemaakt

--- toets/opdracht 2/winkelwagen.js
+++ toets/opdracht 2/winkelwagen.js
@@ -1,4 +1,29 @@
+function Artikel (item, maat, voorraad, kleur, aantal ) {
+    let product = item;
+    let size = maat;
+    let stock = voorraad;
+    let color = kleur;
+    let aantal = aantal;
 
+    this.getItem = function (){
+        return product;
+    }
+    this.getMaat = function () {
+        return size;
+    }
+    this.getVoorraad = function () {
+        return stock;
+    }
+    this.getKleur = function () {
+        return color
+    }
+    this.getAantal = function () {
+        return aantal
+    }
+    this.showDetail = function () {
+        console.log("Maat: " + size + "Kleur: " + color);
+    }
+}
 
 
 
